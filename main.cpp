ecdda0b0f128d2db0afa657bd2fea490c1add8e1
juanpab1980 <juanpab1980@b98271d3-b73c-0410-b3e6-1dfb2072a0f9>
2007-12-18 23:13:44 +0000

    sin solucionar el paso de doble canal a canal sencillo en el osciloscopio y falta agragar una variable para graficar lo del analizador logico

--- main.cpp
+++ main.cpp
@@ -13,10 +13,10 @@
 #include <pthread.h>
 
 Multimetro *mult =0;
-Osciloscopio *osc = 0;
+//Osciloscopio *osc = 0;
 Analizador *ana = 0;
 Generador *gene = 0;
-pthread_t thread, thread1, thread2;
+pthread_t thread, thread1, thread2, thread3;
 
 int status, status2, status3;
 
@@ -25,34 +25,44 @@ void *runhilo(void *threadid)
    mult = new Multimetro();
 }
 
-void *runhilo3(void *threadid)
+
+/*void *runhilo2(void *threadid)
+{
+   osc = new Osciloscopio(8,8,380,304,"",150);
+}*/
+
+
+/*void *runhilo3(void *threadid)
 {
    ana = new Analizador();
 }
 
-void *runhilo2(void *threadid)
+void *runhilo4(void *threadid)
 {
-   osc = new Osciloscopio(8,8,380,304,"",150);
-}
+   gene = new Generador();
+}*/
+
+
 
 int main (int argc, char ** argv)
 {
   Fl_Double_Window *window;
-  Fl_Window *window2;
- 
-  
-  //Fl_Light_Button *omult_on;
+  Analizador *ana;
+  Generador *gene;
+  Osciloscopio *osc;
 
   int t =0;
   int u =0;
   int y =0;
-  int rc, rc2, rc3;
+  int f =0;
+  int rc, rc2, rc3, rc4;
   
   window = new Fl_Double_Window (1024, 708);
-  //window2 = new Fl_Window (104, 108, 200,200,"hola");
   ana = new Analizador();
   gene = new Generador();
+  osc = new Osciloscopio(8,8,380,304,"",150);
   
+  //window->position(0,0);
   
   rc=pthread_create(&thread, NULL, runhilo, (void *)t);
         if (rc){
@@ -60,48 +70,55 @@ int main (int argc, char ** argv)
            exit(-1);
   }
         
-  rc2=pthread_create(&thread1, NULL, runhilo2, (void *)u);
+/*  rc2=pthread_create(&thread1, NULL, runhilo2, (void *)u);
         if (rc2){
            fl_message("ERROR; return code from pthread_create() is %d\n", rc2);
            exit(-1);
-  }
+  }*/
   
  /* rc3=pthread_create(&thread2, NULL, runhilo3, (void *)y);
         if (rc3){
            fl_message("ERROR; return code from pthread_create() is %d\n", rc3);
            exit(-1);
-  }*/       
+  }       
  
+ 
+  rc4=pthread_create(&thread3, NULL, runhilo4, (void *)f);
+        if (rc4){
+           fl_message("ERROR; return code from pthread_create() is %d\n", rc4);
+           exit(-1);
+  }*/
+  
+   
+   
    
   rc = pthread_join(thread, NULL);
-      if (rc)
-      {
+      if (rc){
          printf("ERROR; return code from pthread_join() is %d\n", rc);
          exit(-1);
       } 
       
-  rc2 = pthread_join(thread1, NULL);
-      if (rc2)
-      {
+ /* rc2 = pthread_join(thread1, NULL);
+      if (rc2){
          printf("ERROR; return code from pthread_join() is %d\n", rc2);
          exit(-1);
-      }
+      }*/
       
-/*  rc3= pthread_join(thread2, NULL);
-      if (rc3)
-      {
+ /* rc3= pthread_join(thread2, NULL);
+      if (rc3){
          printf("ERROR; return code from pthread_join() is %d\n", rc3);
          exit(-1);
       }      
-  */ 
-  
+   
+  rc4= pthread_join(thread3, NULL);
+      if (rc4){
+         printf("ERROR; return code from pthread_join() is %d\n", rc4);
+         exit(-1);
+      } */
   
   
   window->end ();
   window->show (argc, argv);
-  
-//  window2->end ();
-//  window2->show (argc, argv);
   
   return(Fl::run());
   
